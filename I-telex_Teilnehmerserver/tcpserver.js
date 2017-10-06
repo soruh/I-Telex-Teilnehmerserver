2aaea48335afa2b41e2c332a5ed68404eb16b27b
soruh <mail@soruh.de>
2017-10-06 22:02:44 +0200

    -

--- I-telex_Teilnehmerserver/tcpserver.js
+++ I-telex_Teilnehmerserver/tcpserver.js
@@ -82,11 +82,9 @@ handles[1][STANDBY] = (obj,cnum,dbcon,connection)=>{
 			if(res.pin==pin&&res.port==port/*???*/){
 				dbcon.query("UPDATE telefonbuch.teilnehmer SET ipaddresse='"+connection.remoteAddress+"' WHERE rufnummer="+number,function(err_b,result_b){
 					dbcon.query("SELECT * FROM telefonbuch.teilnehmer WHERE rufnummer="+number,function(err_c,result_c){
-						console.log(err_c,result_c);
 						try{
-							connection.write(encPacket({packagetype:2,datalength:4,data:{ipaddresse:result_c[0].ipaddresse}}),"binary");
+							connection.write(encPacket({packagetype:2,datalength:4,data:{ipaddresse:result_c[0].ipadresse}}),"binary");
 						}catch(e){
-							console.log("error");
 							console.log(FgRed,e,FgWhite);
 						}
 					});
