af9e6975e35a6ddee05349e08f933c9177100148
soruh <mail@soruh.de>
2017-10-06 23:58:17 +0200

    -

--- I-telex_Teilnehmerserver/queuewatchdog.js
+++ I-telex_Teilnehmerserver/queuewatchdog.js
@@ -1,26 +1,4 @@
-var Reset = "\x1b[0m";
-var Bright = "\x1b[1m";
-var Dim = "\x1b[2m";
-var Underscore = "\x1b[4m";
-var Blink = "\x1b[5m";
-var Reverse = "\x1b[7m";
-var Hidden = "\x1b[8m";
-var FgBlack = "\x1b[30m";
-var FgRed = "\x1b[31m";
-var FgGreen = "\x1b[32m";
-var FgYellow = "\x1b[33m";
-var FgBlue = "\x1b[34m";
-var FgMagenta = "\x1b[35m";
-var FgCyan = "\x1b[36m";
-var FgWhite = "\x1b[37m";
-var BgBlack = "\x1b[40m";
-var BgRed = "\x1b[41m";
-var BgGreen = "\x1b[42m";
-var BgYellow = "\x1b[43m";
-var BgBlue = "\x1b[44m";
-var BgMagenta = "\x1b[45m";
-var BgCyan = "\x1b[46m";
-var BgWhite = "\x1b[47m";
+const Reset = "\x1b[0m",Bright = "\x1b[1m",Dim = "\x1b[2m",Underscore = "\x1b[4m",Blink = "\x1b[5m",Reverse = "\x1b[7m",Hidden = "\x1b[8m",FgBlack = "\x1b[30m",FgRed = "\x1b[31m",FgGreen = "\x1b[32m",FgYellow = "\x1b[33m",FgBlue = "\x1b[34m",FgMagenta = "\x1b[35m",FgCyan = "\x1b[36m",FgWhite = "\x1b[37m",BgBlack = "\x1b[40m",BgRed = "\x1b[41m",BgGreen = "\x1b[42m",BgYellow = "\x1b[43m",BgBlue = "\x1b[44m",BgMagenta = "\x1b[45m",BgCyan = "\x1b[46m",BgWhite = "\x1b[47m";
 
 var functions=require("./functions.js");
 eval(functions);
