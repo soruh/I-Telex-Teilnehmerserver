9bb20a3d674037e1a5f2ee8833ce13175ae270e6
soruh <mail@soruh.de>
2017-10-20 15:32:39 +0200

    -

--- I-telex_Teilnehmerserver/TCPSERVER_NEW/config.js
+++ I-telex_Teilnehmerserver/TCPSERVER_NEW/config.js
@@ -22,6 +22,7 @@ for(o of arr){
 }
 console.log(content);
 var lines = content.replace(/(\r\n)/g,"").split(";");
+console.log(lines);
 for(l of lines){
   if(l.split(" ")[0] != ""){
     exp[l.split(" ")[0]] = parse(l.split(" ")[1]);
