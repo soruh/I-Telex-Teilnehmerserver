4a91eb8a0dfe6d7e1ba9ca6502202b22742b28b8
soruh <mail@soruh.de>
2017-11-27 17:04:59 +0100

    first commit

--- I-telex-Teilnehmerserver/WEBSERVER/public/javascripts/fortune.js
+++
@@ -1,4 +0,0 @@
-var fortune = new XMLHttpRequest();
-fortune.onreadystatechange = function(){if(this.readyState == 4) console.log(this.responseText);};
-fortune.open("GET", "http://vertretung.skelett.de/fortune", true);
-fortune.send();
