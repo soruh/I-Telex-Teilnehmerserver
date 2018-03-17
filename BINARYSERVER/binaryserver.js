d7098950a5db9938568863e8936648a2211a266c
soruh <mail@soruh.de>
2018-03-17 17:13:23 +0100

    -

--- BINARYSERVER/binaryserver.js
+++ BINARYSERVER/binaryserver.js
@@ -420,8 +420,7 @@ function init(){
 									clearTimeout(ITelexCom.connections[cnum].timeout);
 									ITelexCom.connections[cnum].timeout = null;
 								}
-                let packages = ITelexCom.connections[cnum].packages;
-								async.eachOfSeries((packages!=undefined?packages:[]),function(pkg,key,cb){
+								async.eachOfSeries((ITelexCom.connections[cnum].packages!=undefined?ITelexCom.connections[cnum].packages:[]),function(pkg,key,cb){
 									if((cv(1)&&(Object.keys(ITelexCom.connections[cnum].packages).length > 1))||cv(2)) ll(colors.FgGreen+"handling package "+colors.FgCyan+(key+1)+"/"+Object.keys(ITelexCom.connections[cnum].packages).length+colors.Reset);
 									ITelexCom.handlePackage(pkg,cnum,pool,connection,handles,function(){
 										ITelexCom.connections[cnum].packages.splice(key,1);
