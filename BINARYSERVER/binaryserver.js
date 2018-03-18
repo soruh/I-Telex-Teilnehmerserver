1269a7607fb1e0cf4aba43d4906f58ad24ab979c
soruh <mail@soruh.de>
2018-03-18 20:11:23 +0100

    -

--- BINARYSERVER/binaryserver.js
+++ BINARYSERVER/binaryserver.js
@@ -342,6 +342,7 @@ function init(){
 			}
 			ITelexCom.connections[cnum] = {connection:connection,state:ITelexCom.states.STANDBY,handling:false};
 			if(cv(1)) ll(colors.FgGreen+"client "+colors.FgCyan+cnum+colors.FgGreen+" connected with ipaddress: "+colors.FgCyan+connection.remoteAddress+colors.Reset); //.replace(/^.*:/,'')
+			if(connection.remoteAddress == undefined) setTimeout(function(){ll(connection.remoteAddress)},1000);
 			var queryresultpos = -1;
 			var queryresult = [];
 			var connectionpin;
