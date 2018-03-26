97e779838ff64b5232eae4dc07d1037c3b3cc0a3
soruh <mail@soruh.de>
2018-03-26 13:56:22 +0200

    fixed possible hangup on unreachable email server

--- BINARYSERVER/ITelexCom.js
+++ BINARYSERVER/ITelexCom.js
@@ -732,12 +732,14 @@ function sendEmail(transporter,messageName,values,callback){
 	}
   if(cv(2)) ll("sending mail:",mailOptions);
   transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-          return lle(error);
-      }
-      if(cv(1)) ll('Message sent:', info.messageId);
-      if(config.get("eMail").useTestAccount) ll('Preview URL:', nodemailer.getTestMessageUrl(info));
-      if(typeof callback === "function") callback();
+      if (error){
+        lle(error);
+				if(typeof callback === "function") callback();
+      }else{
+	      if(cv(1)) ll('Message sent:', info.messageId);
+	      if(config.get("eMail").useTestAccount) ll('Preview URL:', nodemailer.getTestMessageUrl(info));
+	      if(typeof callback === "function") callback();
+			}
   });
 }
 
