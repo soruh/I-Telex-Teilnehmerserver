80aebaa565a53054a0248d721fe8752750396746
soruh <mail@soruh.de>
2018-05-31 15:16:35 +0200

    added missing )

--- BINARYSERVER/binaryserver.js
+++ BINARYSERVER/binaryserver.js
@@ -78,7 +78,8 @@ handles[1][ITelexCom.states.STANDBY] = function(obj,cnum,pool,connection,handles
 							var res = results[0];
 							if(res.pin == pin){
 								if(res.typ == 5){
-								ITelexCom.SqlQuery(pool,`UPDATE teilnehmer SET port = '${port}', ipaddresse = '${ipaddress}', ${((port!=res.port||ipaddress!=res.ipaddresse)?(",changed = '1', moddate ="+Math.floor(Date.now()/1000)):"")} WHERE rufnummer = ${number} OR (Left(name, ${config.get("DynIpUpdateNameDifference")}) = Left('${res.name}', ${config.get("DynIpUpdateNameDifference")}) AND port = '${res.port}' AND pin = '${res.pin}'`,function(result_b){
+								ITelexCom.SqlQuery(pool,`UPDATE teilnehmer SET port = '${port}', ipaddresse = '${ipaddress}', ${((port!=res.port||ipaddress!=res.ipaddresse)?(",changed = '1', moddate ="+Math.floor(Date.now()/1000)):"")} WHERE rufnummer = ${number} OR
+								 (Left(name, ${config.get("DynIpUpdateNameDifference")}) = Left('${res.name}', ${config.get("DynIpUpdateNameDifference")})) AND port = '${res.port}' AND pin = '${res.pin}'`,function(result_b){
 									ITelexCom.SqlQuery(pool,`SELECT * FROM teilnehmer WHERE rufnummer = ${number};`,function(result_c){
 										try{
 											connection.write(ITelexCom.encPackage({packagetype:2,datalength:4,data:{ipaddresse:result_c[0].ipaddresse}}),"binary",function(){if(typeof cb === "function") cb();});
