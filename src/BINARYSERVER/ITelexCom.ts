3e3abe4e26e16ad1027b5446981288c302aa14c1
soruh <mail@soruh.de>
2018-07-05 21:51:37 +0200

    fixed flags

--- src/BINARYSERVER/ITelexCom.ts
+++ src/BINARYSERVER/ITelexCom.ts
@@ -321,7 +321,7 @@ function decPackageData(packagetype: number, buffer:Buffer|number[]): PackageDat
             data = {
                 number: < number > BytearrayToValue(buffer.slice(0, 4), "number"),
                 name: < string > BytearrayToValue(buffer.slice(4, 44), "string"),
-                disabled: flags[0] & 2,
+                disabled: (flags[0]&2)==2?1:0,
                 type: < number > BytearrayToValue(buffer.slice(46, 47), "number"),
                 hostname: < string > BytearrayToValue(buffer.slice(47, 87), "string"),
                 ipaddress: < string > BytearrayToValue(buffer.slice(87, 91), "ip"),
