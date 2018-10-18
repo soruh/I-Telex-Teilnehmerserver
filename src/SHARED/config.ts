a7d631a97b050b70fc9b64422a4f16c117dc8835
soruh <mail@soruh.de>
2018-10-18 20:45:28 +0200

    fixed indent in config.ts

--- src/SHARED/config.ts
+++ src/SHARED/config.ts
@@ -1,104 +1,104 @@
 interface configFile {
-		mySqlConnectionOptions: {
+	mySqlConnectionOptions: {
+		host: string,
+		user: string,
+		password: string,
+		database: string,
+		insecureAuth ? : boolean,
+
+		connectTimeout ? : number,
+		aquireTimeout ? : number,
+		timeout ? : number
+	};
+	DynIpUpdateNameDifference: number;
+
+	setChangedOnNewerEntry: boolean;
+	allowInvalidPackageSizes: boolean;
+
+	logLineNumbers: boolean;
+	logDate: boolean;
+	highlightSqlQueries:boolean;
+	bufferLogWithWhitespace: boolean;
+	repairPm2Colors: boolean;
+	logFullBuffer: boolean;
+	explainBuffers: number;
+
+	logConnectionChanges: boolean;
+	serverPin: number;
+	allowFullQueryInReadonly: boolean;
+	allowLoginInReadonly: boolean;
+	doDnsLookups: boolean;
+	// updateQueueInterval: number,
+	cleanUpInterval:number;
+	queueSendInterval: number;
+	fullQueryInterval: number;
+	fullQueryServer: string;
+	connectionTimeout: number;
+
+	binaryserverLog: string;
+	binaryserverErrorLog: string;
+	logBinaryserverToConsole: boolean;
+
+	keepDeletedFor:number;
+	webserverLog: string;
+	webserverErrorLog: string;
+	logWebserverToConsole: boolean;
+
+	binaryPort: number;
+	binaryserverLoggingLevel: number | string;
+	webserverLoggingLevel: number | string;
+	disableColors: boolean;
+
+	webServerTokenLifeTime: number;
+	webServerPort: number;
+	webInterfacePassword: string;
+	
+	warnAtErrorCounts: number[];
+	scientistNames:boolean;
+
+	eMail: {
+		useTestAccount: boolean,
+		account: {
 			host: string,
-			user: string,
-			password: string,
-			database: string,
-			insecureAuth ? : boolean,
-
-			connectTimeout ? : number,
-			aquireTimeout ? : number,
-			timeout ? : number
-		};
-		DynIpUpdateNameDifference: number;
-
-		setChangedOnNewerEntry: boolean;
-		allowInvalidPackageSizes: boolean;
-
-		logLineNumbers: boolean;
-		logDate: boolean;
-		highlightSqlQueries:boolean;
-		bufferLogWithWhitespace: boolean;
-		repairPm2Colors: boolean;
-		logFullBuffer: boolean;
-		explainBuffers: number;
-
-		logConnectionChanges: boolean;
-		serverPin: number;
-		allowFullQueryInReadonly: boolean;
-		allowLoginInReadonly: boolean;
-		doDnsLookups: boolean;
-		// updateQueueInterval: number,
-		cleanUpInterval:number;
-		queueSendInterval: number;
-		fullQueryInterval: number;
-		fullQueryServer: string;
-		connectionTimeout: number;
-
-		binaryserverLog: string;
-		binaryserverErrorLog: string;
-		logBinaryserverToConsole: boolean;
-
-		keepDeletedFor:number;
-		webserverLog: string;
-		webserverErrorLog: string;
-		logWebserverToConsole: boolean;
-
-		binaryPort: number;
-		binaryserverLoggingLevel: number | string;
-		webserverLoggingLevel: number | string;
-		disableColors: boolean;
-
-		webServerTokenLifeTime: number;
-		webServerPort: number;
-		webInterfacePassword: string;
-		
-		warnAtErrorCounts: number[];
-		scientistNames:boolean;
-
-		eMail: {
-			useTestAccount: boolean,
-			account: {
-				host: string,
-				port: number,
-				secure: boolean,
-				auth: {
-					user: string,
-					pass: string
-				},
-				tls: {
-					rejectUnauthorized: boolean
-				}
+			port: number,
+			secure: boolean,
+			auth: {
+				user: string,
+				pass: string
 			},
-			to: string,
-			from: string,
-			messages: {
-				new: {
-					subject: string,
-					html: string
-				},
-				invalidNumber: {
-					subject: string,
-					html: string
-				},
-				wrongDynIpPin: {
-					subject: string,
-					html: string
-				},
-				wrongDynIpType: {
-					subject: string,
-					html: string
-				},
-				wrongServerPin: {
-					subject: string,
-					html: string
-				},
-				ServerError: {
-					subject: string,
-					html: string
-				}
+			tls: {
+				rejectUnauthorized: boolean
 			}
-		};
+		},
+		to: string,
+		from: string,
+		messages: {
+			new: {
+				subject: string,
+				html: string
+			},
+			invalidNumber: {
+				subject: string,
+				html: string
+			},
+			wrongDynIpPin: {
+				subject: string,
+				html: string
+			},
+			wrongDynIpType: {
+				subject: string,
+				html: string
+			},
+			wrongServerPin: {
+				subject: string,
+				html: string
+			},
+			ServerError: {
+				subject: string,
+				html: string
+			}
+		}
+	};
 }
 
 let collection: any = {};
