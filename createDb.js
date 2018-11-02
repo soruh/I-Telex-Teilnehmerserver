e8d6dd159029d1c5c986322d3d4d065ac7a26669
soruh <mail@soruh.de>
2018-11-02 22:31:06 +0100

    improved output of createDb.js

--- createDb.js
+++ createDb.js
@@ -4,32 +4,46 @@ const fs = require("fs");
 const config = require("./src/SHARED/config.js").default;
 
 const dbPath = path.isAbsolute(config.DBPath)?config.DBPath:path.join(__dirname, config.DBPath);
+const dbExists = fs.existsSync(dbPath);
+const dbPathExists = fs.existsSync(path.dirname(dbPath));
+
+
+if(dbPathExists){
+	console.log('database directory already exists');
+}else{
+	fs.mkdirSync(path.dirname(dbPath), {recursive: true});
+	console.log('created database directory');
+}
+
+if(dbExists){
+	console.log('database file already exists');
+}
+
+connectToDb()
+.then(db=>new Promise((resolve, reject)=>{
+	db.exec(fs.readFileSync('./tables.sql').toString(), err=>{
+		if(err){
+			reject(err);
+			return;
+		}
+		console.log('created tables');
+		resolve();
+	});
+}))
+.catch((err) => {
+	console.error(err.message);
+});
+
 
 function connectToDb() {
     return new Promise((resolve, reject) => {
-        let db = new sqlite.Database(dbPath, (sqlite.OPEN_CREATE|sqlite.OPEN_READWRITE), err => {
+        const db = new sqlite.Database(dbPath, (sqlite.OPEN_CREATE|sqlite.OPEN_READWRITE), err => {
             if (err) {
                 reject(err);
                 return;
 			}
-			console.log('opened database.');
+			if(!dbExists) console.log('created database file');
             resolve(db);
         });
     });
 }
-
-fs.mkdirSync(path.dirname(dbPath), {recursive: true});
-
-connectToDb()
-.then((db) => {
-	db.exec(fs.readFileSync('./tables.sql').toString(), (err)=>{
-		if(err) {
-			console.error(err);
-			process.exit(1);
-		}
-		console.log('created tables.');
-	});
-})
-.catch((err) => {
-	console.error(err);
-});
