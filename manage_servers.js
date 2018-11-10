137e6adfa33936487a72addcccef2b0d17126aa1
soruh <mail@soruh.de>
2018-11-10 22:21:03 +0100

    minor internal changed to manages_servers.js

--- manage_servers.js
+++ manage_servers.js
@@ -15,7 +15,7 @@ if (process.argv[2] == "--help") {
 				if (process.argv.length == 6) {
 					db.run("INSERT INTO servers (address, port, version) VALUES (?, ?, ?);", [process.argv[3], process.argv[4], process.argv[5]], function (err) {
 						if (err) {
-							console.error(err);
+							throw(err);
 						} else {
 							console.log("done!");
 							console.log(`Added entry:\n${process.argv[3]} ${process.argv[4]} ${process.argv[5]}`);
@@ -30,7 +30,7 @@ if (process.argv[2] == "--help") {
 				if (process.argv.length == 6) {
 					db.run("DELETE FROM servers WHERE address=? AND port=? AND version=?;", [process.argv[3], process.argv[4], process.argv[5]], function (err) {
 						if (err) {
-							console.error(err);
+							throw(err);
 						} else if (this.changes > 0) {
 							console.log(`done!\ndeleted entrys: ${this.changes}`);
 						} else {
@@ -46,7 +46,7 @@ if (process.argv[2] == "--help") {
 				if (process.argv.length == 3) {
 					db.all("SELECT * FROM servers;", function (err, entries) {
 						if (err) {
-							console.error(err);
+							throw(err);
 						} else {
 							for (let entry of entries) {
 								console.log(`${entry.address} ${entry.port} ${entry.version}`);
@@ -65,6 +65,27 @@ if (process.argv[2] == "--help") {
 }
 
 function printUsage() {
-	console.log("USAGE: " + process.argv[0].split("/").slice(-1)[0] + " " + process.argv[1].split("/").slice(-1)[0] + " [OPTION] (parameter1) (parameter2)\n\nSYNOPSIS:\n\Manage servers\n\nOPTIONS:\n\nlist:\n\tList all servers\n\tUSAGE: " + process.argv[0].split("/").slice(-1)[0] + " " + process.argv[1].split("/").slice(-1)[0] + " list\n\nadd:\n\tAdd a server\n\tUSAGE: " + process.argv[0].split("/").slice(-1)[0] + " " + process.argv[1].split("/").slice(-1)[0] + " add [server address] [server port] [server version]\n\nremove:\n\tRemove a server\n\tUSAGE: " + process.argv[0].split("/").slice(-1)[0] + " " + process.argv[1].split("/").slice(-1)[0] + " remove [server address] [server port] [server version]\n");
+	const programName = process.argv[0].split("/").slice(-1)[0]+' '+process.argv[1].split("/").slice(-1)[0];
+	console.log(
+`USAGE: ${programName} [OPTION] [...arguments]
+
+SYNOPSIS:
+Manage servers
+
+OPTIONS:
+
+list:
+	List all servers
+	USAGE: ${programName} list
+
+add:
+	Add a server
+	USAGE: ${programName} add [server address] [server port] [server version]
+
+
+remove:
+	Remove a server
+	USAGE: ${programName} remove [server address] [server port] [server version]
+`);
 	process.exit(1);
 }
