235a4e056a3235e666c3f329af4de8586995fbae
soruh <mail@soruh.de>
2019-01-07 23:38:07 +0100

    also exit compileDocker without docker

--- tools/compileDocker.sh
+++ tools/compileDocker.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+#check if docker is installed
+DOCKER_PATH=`which docker`
+if [ $? -eq 1 ]
+then
+	echo "please install docker"
+	exit 1
+fi
+
 # go to project directory
 dir="`pwd`/`dirname "$0"`"
 cd $dir;
