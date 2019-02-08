bef555acb18ab46bb54fcde16de901c63ed88158
soruh <mail@soruh.de>
2019-02-08 15:28:37 +0100

    init_mysql.js should now work.

--- tools/init_mysql.js
+++ tools/init_mysql.js
@@ -1,17 +1,24 @@
 #!/usr/bin/env node
 "use strict";
+global.logger = {
+	log: (level, message)=>{
+		console.log(message);
+	}
+};
+const fs = require('fs');
 const path = require('path');
-const sql = require(path.join(__dirname, '../src/shared/SQL'));
 const config = require(path.join(__dirname, '../src/shared/config')).default;
+
+let oldConfig = {};
+for(let i in config.mysql) oldConfig[i] = config.mysql[i];
 
 config.useMysql = true;
 config.mysql.multipleStatements = true;
 
-global.logger = console;
+const sql = require(path.join(__dirname, '../src/shared/SQL'));
 
-logger.error('\x1b[31mThis script is W.I.P.\x1b[0m');
 
-function getInput(message, muted){ // TODO: use
+function getInput(message, muted){
 	return new Promise((resolve, reject) => {
 		const readline = require('readline');
 		const { Writable } = require('stream');
@@ -41,8 +48,10 @@ function getInput(message, muted){ // TODO: use
 	});
 }
 
-async function connectToDb(){
+async function connectToDb(promtForUser){
 	try{
+		if(promtForUser) throw({code: 'ECONNREFUSED'});
+
 		await sql.connectToDb();
 	}catch(err){
 		if(err.code !== 'ECONNREFUSED') throw(err);
@@ -50,23 +59,67 @@ async function connectToDb(){
 		const username = await getInput('Enter username', false);
 		const password = await getInput('Enter password', true);
 
-		config.mysql.user = username;
+		config.mysql.user     = username;
 		config.mysql.password = password;
 		
 		try{
 			await sql.connectToDb();
 		}catch(err){
-			if(err.code === 'ECONNREFUSED'){
-				console.log("connection refused");
-			}else throw(err);
+			throw(err);
 		}
 	}
 }
 
+function createDatabase(){
+	return sql.run("CREATE DATABASE ?;", [oldConfig.database]);
+}
+
+function createUser(){
+	return sql.run("CREATE USER ?@localhost IDENTIFIED BY ?;", [oldConfig.user, oldConfig.password]);
+}
+
+function createGrant(){
+	return sql.run("GRANT ALL ON ?.* TO ?@localhost;", [oldConfig.database, oldConfig.user]);
+}
+function createTables(){
+	return sql.run(fs.readFileSync(path.join(__dirname, '../tables.sql')));
+}
+
+
 (async ()=>{
 	try{
-		await connectToDb();
+		await connectToDb(process.argv[2]);
+		console.log("\x1b[32mconnect to database\x1b[0m");
 	}catch(err){
-		throw(err);
+		console.error("\x1b[31mcould not connect to database\x1b[0m");
+		process.exit();
 	}
-});
+
+	try{
+		await createDatabase();
+		console.log("\x1b[32mcreated: database\x1b[0m");
+	}catch(err){
+		console.error("\x1b[31mcould not create: database\x1b[0m");
+	}
+
+	try{
+		await createTables();
+		console.log("\x1b[32mcreated: tables\x1b[0m");
+	}catch(err){
+		console.error("\x1b[31mcould not create: tables\x1b[0m");
+	}
+
+	try{
+		await createUser();
+		console.log("\x1b[32mcreated: user\x1b[0m");
+	}catch(err){
+		console.error("\x1b[31mcould not create: user\x1b[0m");
+	}
+
+	try{
+		await createGrant();
+		console.log("\x1b[32mcreated: grant\x1b[0m");
+	}catch(err){
+		console.error("\x1b[31mcould not create: grant\x1b[0m");
+	}
+})();
