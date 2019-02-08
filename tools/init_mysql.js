4efe5f992a864f2d4a13d50999df7cfcfd056f3a
soruh <mail@soruh.de>
2019-02-08 14:50:04 +0100

    init_mysql should now connect correctly

--- tools/init_mysql.js
+++ tools/init_mysql.js
@@ -1,39 +1,43 @@
 #!/usr/bin/env node
 "use strict";
-process.env.USE_MYSQL = 1;
+const path = require('path');
+const sql = require(path.join(__dirname, '../src/shared/SQL'));
+const config = require(path.join(__dirname, '../src/shared/config')).default;
 
-let path = require('path');
-let sql = require(path.join(__dirname, '../src/shared/SQL'));
+config.useMysql = true;
+config.mysql.multipleStatements = true;
 
 global.logger = console;
 
-function getPassword(){ // TODO: use
-	return new Promise((resolve, reject) => {
-		var readline = require('readline');
-		var { Writable } = require('stream');
+logger.error('\x1b[31mThis script is W.I.P.\x1b[0m');
 
-		var mutableStdout = new Writable({
+function getInput(message, muted){ // TODO: use
+	return new Promise((resolve, reject) => {
+		const readline = require('readline');
+		const { Writable } = require('stream');
+
+		const mutableStdout = new Writable({
 			write: function(chunk, encoding, callback) {
-				if (!this.muted)
-				process.stdout.write(chunk, encoding);
+				if (!this.muted) process.stdout.write(chunk, encoding);
 				callback();
 			}
 		});
 
 		mutableStdout.muted = false;
 
-		var rl = readline.createInterface({
+		const rl = readline.createInterface({
 			input: process.stdin,
 			output: mutableStdout,
-			terminal: true
+			terminal: true,
 		});
 
-		rl.question('Enter password: ', function(password) {
-			resolve(password);
+		rl.question(message+': ', function(password) {
 			rl.close();
+			if(muted) process.stdout.write('\n');
+			resolve(password);
 		});
 
-		mutableStdout.muted = true;
+		mutableStdout.muted = muted;
 	});
 }
 
@@ -41,8 +45,28 @@ async function connectToDb(){
 	try{
 		await sql.connectToDb();
 	}catch(err){
-		console.log(err.code);
+		if(err.code !== 'ECONNREFUSED') throw(err);
+
+		const username = await getInput('Enter username', false);
+		const password = await getInput('Enter password', true);
+
+		config.mysql.user = username;
+		config.mysql.password = password;
+		
+		try{
+			await sql.connectToDb();
+		}catch(err){
+			if(err.code === 'ECONNREFUSED'){
+				console.log("connection refused");
+			}else throw(err);
+		}
 	}
 }
 
-connectToDb();
+(async ()=>{
+	try{
+		await connectToDb();
+	}catch(err){
+		throw(err);
+	}
+});
